572cc5a241b7e37bc9e912d3e75071e589a589d0
Trey <tbeau5595@hotmail.com>
2018-10-24 23:27:23 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,4 +2,11 @@
 # out the current state.
 
 def display_board
-  puts 
+  puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]}"
+end
+
+board = []
