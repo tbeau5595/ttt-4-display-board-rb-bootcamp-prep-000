7b68b5ba1b27721d2fe9a3330e5a7cb060150a4c
Trey <tbeau5595@hotmail.com>
2018-10-24 23:35:39 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,4 +9,4 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]}"
 end
 
-board = [" ", "X", " ", " ", " ", " ", " ", " ", " ", ]
+board = [" ", "", " ", " ", "X", " ", " ", " ", " ", ]
